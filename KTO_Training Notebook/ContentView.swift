25acd8286aa9eaaf63c1bdf8e98bce535013a97b
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-10-08 15:33:04 +0300

    Initial Commit

---
+++ KTO_Training Notebook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KTO_Training Notebook
+//
+//  Created by Роман on 08.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KTO_Training Notebook/KTO_Training_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  KTO_Training_NotebookApp.swift
+//  KTO_Training Notebook
+//
+//  Created by Роман on 08.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct KTO_Training_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
